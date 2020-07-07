58d667bcea7a8e28d3c5a97c4b95910da682e0f7
Roma <miheev.roma90@mail.ru>
2020-07-07 15:17:21 +0300

    .

--- ClozeQuesConverter/Program.cs
+++ ClozeQuesConverter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -7,34 +8,86 @@ namespace ClozeQuesConverter
 {
     class Program
     {
-        static readonly Regex beginRegex = new Regex(@"<begin name=.*; sa=[0|1]>");
+        static readonly Regex beginRegex = new Regex(@"<begin name=(.*); sa=([0|1])>");
         static readonly Regex endRegex = new Regex(@"<end>");
 
-        static string GetNextQuestion(StreamReader input)
+        static readonly string xamlPattern =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?><quiz>(.*)</quiz>";
+
+        /// <summary>
+        /// 1-ая группа - название вопроса (Name)
+        /// 2-ая группа - сам вопрос (Body)
+        /// 3-яя группа - ShuffleAnswers
+        ///</summary>
+        static readonly string questPattern =
+            "<question type=\"cloze\">" +
+            "<name><text>(.*)</text>" +
+            "</name>" +
+            "<questiontext>" +
+            "<text><![CDATA[(.*)]]></text>" +
+            "</questiontext>" +
+            "<generalfeedback>" +
+            "<text></text>" +
+            "</generalfeedback>" +
+            "<shuffleanswers>(.*)</shuffleanswers>" +
+            "</question>";
+
+
+        /// <summary>
+        /// Ищет следующий вопрос в потоке input
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>Объект типа Question, если не нашлось, то null</returns>
+        static Question GetNextQuestion(StreamReader input)
         {
+            string name = string.Empty;
+            bool shuffleAnswers = false;
             while (input.EndOfStream == false)
             {
                 string curString = input.ReadLine();
                 if (beginRegex.IsMatch(curString))
+                {
+                    //init name and shaffleAnswers 
+                    var groups = beginRegex.Match(curString).Groups;
+                    name = groups[1].Value;
+                    shuffleAnswers = groups[2].Value == "1" ? true : false;
                     break;
+                }
             }
-            var question = new StringBuilder();
+            var body = new StringBuilder();
             while (input.EndOfStream == false)
             {
                 string curString = input.ReadLine();
                 if (endRegex.IsMatch(curString) == false)
-                    question.Append(curString);
+                    body.Append(curString);
                 else break;
             }
-            return question.ToString();
+            var bodyStr = body.ToString();
+            if (string.IsNullOrEmpty(bodyStr))
+                return null;
+            return new Question(name, bodyStr, shuffleAnswers);
         }
+
+
+
         static void Main(string[] args)
         {
             var input = new StreamReader("test.txt");
             var output = new StreamWriter("output.xml");
 
-            Console.WriteLine(GetNextQuestion(input));
-            Console.WriteLine(GetNextQuestion(input));
+            var questions = new List<Question>();
+            while(input.EndOfStream == false)
+            {
+                var nextQuestion = GetNextQuestion(input);
+                if (nextQuestion != null)
+                    questions.Add(nextQuestion);
+            }
+
+            Console.WriteLine(
+                string.Join("\n", questions)
+                );
+
+            Console.ReadKey();
         }
     }
 }
---
+++ ClozeQuesConverter/Question.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClozeQuesConverter
+{
+    class Question
+    {
+        public string Name { get; }
+        public string Body { get; }
+        public bool ShuffleAnswers { get; }
+
+        public Question(string name, string body, bool shuffleAnswers)
+        {
+            Name = name;
+            Body = body;
+            ShuffleAnswers = shuffleAnswers;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("\nName:{0} sa:{1}\n{2}",
+                Name, ShuffleAnswers ? 1 : 0, Body);
+        }
+    }
+}
