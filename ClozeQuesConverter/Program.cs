38cf5cdc39689e8c79c2fb5ca3b020dd0b66a958
Takesomen <67959492+Takesomen@users.noreply.github.com>
2020-07-15 11:07:13 +0300

    Update Program.cs

--- ClozeQuesConverter/Program.cs
+++ ClozeQuesConverter/Program.cs
@@ -213,79 +213,17 @@ namespace ClozeQuesConverter
             return true;
         }
 
-        static readonly string[] shortanswers =
-            new[] { "SHORTANSWER", "SA", "MW", "SHORTANSWER_C", "SAC", "MWC" };
-        static bool IsShortanswer(string str)
-        {
-            //var regex2 = new Regex(@"^{\d+:(?<type>[A-Z_]+):(?<answers>(=|-?%\d+%).+?(#.+)?(~(=|-?%\d+%).+?(#.+?)?)*)}$");
-
-            var regex = new Regex(@"^{\d+:(?<type>[A-Z_]+):(?<answers>(=|-?%\d+%).*[^\\](#.+)?(~(=|-?%\d+%).*[^\\](#.+?)?)*)}$");
-
-            //var regex1 = new Regex(@"^{\d+:([A-Z_]+):(=|%\d+%)[^~{}#]+(#[^~{}#]+)?(~(=|%\d+%)[^~{}#]+(#[^~{}#]+)?)*}$");
-
-            if (regex.IsMatch(str) == false)
-                return false;
-            var groups = regex.Match(str).Groups;
-            if (shortanswers.Contains(groups["type"].Value) == false)
-                return false;
-
-            //this shit doesn't work
-            var answersGroup = regex.Match(str).Groups["answers"].Value;
-            answersGroup = new Regex(@"\\[#~/""\\]").Replace(answersGroup, "");
-            var answers = answersGroup.Split('~', StringSplitOptions.RemoveEmptyEntries);
-
-            return true;
-        }
-
-        static void Check3(string s)
+        static void SearchingMistakes(string s, ref int lineCount)
         {
             int i = 0;
             while (i <= s.Length - 1)
             {
                 if ((s[i] == '}') || (s[i] == '#') || (s[i] == '~') || (s[i] == '/') || (s[i] == '\"') || (s[i] == '\"'))
-                    Console.WriteLine("Обратите внимание на "+i+"-ый символ!("+s[i]+")Так как он может нести двойное значение.");
+                    Console.WriteLine("Обратите внимание на "+i+"-ый символ в "+lineCount+"-ой строке!("+s[i]+")Так как он может нести двойное значение.");
                 i++;
             }
         }
-        static void Check1(string s)
-        {
-            Regex regex = new Regex(@"{[0-9]{1,}:SHORTANSWER:(=|%).+}");
-            Regex regex1 = new Regex(@"{[0-9]{1,}:SHORTANSWER_C:(=|%).+}");
-            Regex regex2 = new Regex(@"{[0-9]{1,}:NUMERICAL:(=|%)[0-9]{1,}}");
-            Regex regex3 = new Regex(@"{[0-9]{1,}:MULTICHOICE:.*(=|%)+.+~+.+}");
-            Regex regex4 = new Regex(@"{[0-9]{1,}:MULTICHOICE_V:.*(=|%)+.+~+.+}");
-            Regex regex5 = new Regex(@"{[0-9]{1,}:MULTICHOICE_H:.*(=|%)+.+~+.+}");
-            Regex regex6 = new Regex(@"{[0-9]{1,}:MULTIRESPONSE:.*(=|%)+.+~+.+}");
-            Regex regex7 = new Regex(@"{[0-9]{1,}:MULTIRESPONSE_H:.*(=|%)+.+~+.+}");
-            Regex regex8 = new Regex(@"{[0-9]{1,}:MULTICHOICE_S:.*(=|%)+.+~+.+}");
-            Regex regex9 = new Regex(@"{[0-9]{1,}:MULTICHOICE_VS :.*(=|%)+.+~+.+}");
-            Regex regex10 = new Regex(@"{[0-9]{1,}:MULTICHOICE_HS :.*(=|%)+.+~+.+}");
-            Regex regex11 = new Regex(@"{[0-9]{1,}:MULTIRESPONSE_S :.*(=|%)+.+~+.+}");
-            Regex regex12 = new Regex(@"{[0-9]{1,}:MULTIRESPONSE_HS :.*(=|%)+.+~+.+}");
-            MatchCollection matches = regex.Matches(s);
-            MatchCollection matches1 = regex1.Matches(s);
-            MatchCollection matches2 = regex2.Matches(s);
-            MatchCollection matches3 = regex3.Matches(s);
-            MatchCollection matches4 = regex4.Matches(s);
-            MatchCollection matches5 = regex5.Matches(s);
-            MatchCollection matches6 = regex6.Matches(s);
-            MatchCollection matches7 = regex7.Matches(s);
-            MatchCollection matches8 = regex8.Matches(s);
-            MatchCollection matches9 = regex9.Matches(s);
-            MatchCollection matches10 = regex10.Matches(s);
-            MatchCollection matches11 = regex11.Matches(s);
-            MatchCollection matches12 = regex12.Matches(s);
-            int sum = matches.Count + matches1.Count + matches2.Count + matches3.Count + matches4.Count + matches5.Count + matches6.Count + matches7.Count + matches8.Count + matches9.Count + matches10.Count + matches11.Count + matches12.Count;
-            if (sum > 0)
-            {
-                Console.WriteLine("Выражение задано правильно");
-            }
-            else
-            {
-                Console.WriteLine("Выражение задано неправильно!");
-            }
-        }
-        //
+        
         
         static void Main(string[] args)
         {
